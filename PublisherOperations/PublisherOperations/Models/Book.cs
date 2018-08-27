ca7bfc61f38f3fb57e46a3c6459c0ad993c348ae
Fagun Rain <mdabuzaforfagun@gmail.com>
2018-08-28 02:57:42 +0600

    use formatter to format string for book model

--- PublisherOperations/PublisherOperations/Models/Book.cs
+++ PublisherOperations/PublisherOperations/Models/Book.cs
@@ -20,9 +20,9 @@ namespace PublisherOperations.Models
         public override void Print()
         {
             Console.WriteLine("Printing Book...");
-            Console.WriteLine($"Title: ${Title}");
-            Console.WriteLine($"Author: ${Author}");
-            Console.WriteLine($"Description: ${Description}");
+            Console.WriteLine(Formatter.Format("Title", Title));
+            Console.WriteLine(Formatter.Format("Author", Author));
+            Console.WriteLine(Formatter.Format("Description", Description));
             Console.WriteLine("-------------------------------");
         }
     }
