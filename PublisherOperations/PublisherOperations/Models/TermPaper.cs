3d5cd731acdf9077f3cf3da31344451a5e784ee0
Fagun Rain <mdabuzaforfagun@gmail.com>
2018-08-27 23:27:50 +0600

    Add term paper model

---
+++ PublisherOperations/PublisherOperations/Models/TermPaper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PublisherOperations.Models
+{
+    public class TermPaper
+    {
+        public string Title { get; set; }
+        public string StudentId { get; set; }
+        public string Refference { get; set; }
+        public string Description { get; set; }
+
+    }
+}
