0553c324e215bc031da482cf1dcc0b0b65cae0a9
Fagun Rain <mdabuzaforfagun@gmail.com>
2018-08-28 02:58:00 +0600

    Add uppercase formatter

---
+++ PublisherOperations/PublisherOperations/Models/UppercaseFormatter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PublisherOperations.Models
+{
+    public class UppercaseFormatter : IFormatter
+    {
+        public string Format(string key, string value)
+        {
+            return $"{key} : {value.ToUpper()}";
+        }
+    }
+}
