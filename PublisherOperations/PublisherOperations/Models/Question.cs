a974246d01fb78bbdf6cfb69bce015052853e330
Fagun Rain <mdabuzaforfagun@gmail.com>
2018-08-27 23:37:56 +0600

    add print method to questions

--- PublisherOperations/PublisherOperations/Models/Question.cs
+++ PublisherOperations/PublisherOperations/Models/Question.cs
@@ -2,7 +2,15 @@
 {
     public class Question
     {
+        public int QuestionId { get; set; }
         public string QuestionText { get; set; }
         public string Answer { get; set; }
+
+        public void Print()
+        {
+            System.Console.WriteLine($"Q No: ${QuestionId}");
+            System.Console.WriteLine($"Q: ${QuestionText}");
+            System.Console.WriteLine($"A: ${Answer}");
+        }
     }
 }
