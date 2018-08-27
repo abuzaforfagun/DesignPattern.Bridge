c2f3361a0c38e7e442517c22348df86ed13ac2a6
Fagun Rain <mdabuzaforfagun@gmail.com>
2018-08-28 00:07:18 +0600

    Do pritning operation

--- PublisherOperations/PublisherOperations/Program.cs
+++ PublisherOperations/PublisherOperations/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PublisherOperations.Models;
+using System;
+using System.Collections.Generic;
 
 namespace PublisherOperations
 {
@@ -6,7 +8,44 @@ namespace PublisherOperations
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Book bookLiveLife = new Book()
+            {
+                Title = "How to live life",
+                Author = "James",
+                Description = "A book on life"
+            };
+            bookLiveLife.Print();
+
+            QuestionBank questionBank = new QuestionBank()
+            {
+                Subject = "C#",
+                Questions = new List<Question>()
+                {
+                    new Question()
+                    {
+                        QuestionId = 1,
+                        QuestionText = "Current version of C#?",
+                        Answer = "7"
+                    },
+                    new Question()
+                    {
+                        QuestionId = 2,
+                        QuestionText = "Can we pass mutliple ",
+                        Answer = "Yes, we can pass though tuple."
+                    }
+                }
+            };
+            questionBank.Print();
+
+            Book bookDesignPatters = new Book()
+            {
+                Title = "Design Patterns",
+                Author = "GOF",
+                Description = "23 design patterns"
+            };
+            bookDesignPatters.Print();
+            Console.Read();
         }
+
     }
 }
