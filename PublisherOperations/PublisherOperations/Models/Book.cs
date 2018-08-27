923e6b8f8d43eb4591c95d3ab247049acb2998aa
Fagun Rain <mdabuzaforfagun@gmail.com>
2018-08-27 23:25:34 +0600

    add book model

---
+++ PublisherOperations/PublisherOperations/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PublisherOperations.Models
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+
+    }
+}
