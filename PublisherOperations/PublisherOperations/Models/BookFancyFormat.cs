4880ed17395c39f239d4874bdcd5650011b676a6
Fagun Rain <mdabuzaforfagun@gmail.com>
2018-08-28 01:32:26 +0600

    fix tab on writeline

--- PublisherOperations/PublisherOperations/Models/BookFancyFormat.cs
+++ PublisherOperations/PublisherOperations/Models/BookFancyFormat.cs
@@ -7,16 +7,24 @@ namespace PublisherOperations.Models
 {
     public class BookFancyFormat : IPrintingItem
     {
+
         public string Title { get; set; }
         public string Author { get; set; }
         public string Description { get; set; }
 
+        public BookFancyFormat(string title, string author, string description)
+        {
+            Title = title;
+            Author = author;
+            Description = description;
+        }
+
         public void Print()
         {
             Console.WriteLine("Printing Book...");
-            Console.WriteLine($"/tTitle: ${Title}");
-            Console.WriteLine($"/tAuthor: ${Author}");
-            Console.WriteLine($"/tDescription: ${Description}");
+            Console.WriteLine($"\tTitle: ${Title}");
+            Console.WriteLine($"\tAuthor: ${Author}");
+            Console.WriteLine($"\tDescription: ${Description}");
             Console.WriteLine("-------------------------------");
         }
     }
