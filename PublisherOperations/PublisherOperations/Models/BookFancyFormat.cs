a98b86a7947c4001022be5ba55715418df04fd59
Fagun Rain <mdabuzaforfagun@gmail.com>
2018-08-28 00:38:21 +0600

    add fancy format book model

---
+++ PublisherOperations/PublisherOperations/Models/BookFancyFormat.cs
@@ -0,0 +1,23 @@
+﻿using PublisherOperations.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PublisherOperations.Models
+{
+    public class BookFancyFormat : IPrintingItem
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine("Printing Book...");
+            Console.WriteLine($"/tTitle: ${Title}");
+            Console.WriteLine($"/tAuthor: ${Author}");
+            Console.WriteLine($"/tDescription: ${Description}");
+            Console.WriteLine("-------------------------------");
+        }
+    }
+}
