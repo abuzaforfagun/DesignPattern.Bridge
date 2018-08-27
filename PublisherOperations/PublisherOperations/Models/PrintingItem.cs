87d3949f1c44049f215678a65ed30c8476e54a7a
Fagun Rain <mdabuzaforfagun@gmail.com>
2018-08-28 02:51:49 +0600

    generate iformatter from constructor

--- PublisherOperations/PublisherOperations/Models/PrintingItem.cs
+++ PublisherOperations/PublisherOperations/Models/PrintingItem.cs
@@ -2,6 +2,11 @@
 {
     public abstract class PrintingItem
     {
+        public IFormatter Formatter { get; set; }
+        public PrintingItem(IFormatter formatter = null)
+        {
+            Formatter = formatter ?? new BasicFormatter();
+        }
         public abstract void Print();
     }
 }
